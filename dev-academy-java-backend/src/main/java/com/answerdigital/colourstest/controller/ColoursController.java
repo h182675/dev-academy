9a21ed4006290fa6f88941782b786ab75e3722fa
Daniel pettersen <d.pettersen@gmail.com>
2019-07-04 14:30:40 +0200

    Created TODO for optional tasks

--- dev-academy-java-backend/src/main/java/com/answerdigital/colourstest/controller/ColoursController.java
+++ dev-academy-java-backend/src/main/java/com/answerdigital/colourstest/controller/ColoursController.java
@@ -1,5 +1,6 @@
 package com.answerdigital.colourstest.controller;
 
+import com.answerdigital.colourstest.exception.NotImplementedException;
 import com.answerdigital.colourstest.model.Colour;
 import com.answerdigital.colourstest.repository.ColoursRepository;
 import java.util.List;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,4 +23,18 @@ public class ColoursController {
     public ResponseEntity<List<Colour>> getColours() {
         return new ResponseEntity(coloursRepository.findAll(), HttpStatus.OK);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Colour> getColour(){
+        // TODO Optional 1
+        throw new NotImplementedException();
+    }
+
+    @PostMapping
+    public ResponseEntity<Colour> createColour(){
+        // TODO Optional 2
+        throw new NotImplementedException();
+    }
+
+
 }
--- dev-academy-java-backend/src/main/java/com/answerdigital/colourstest/controller/PeopleController.java
+++ dev-academy-java-backend/src/main/java/com/answerdigital/colourstest/controller/PeopleController.java
@@ -6,12 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.answerdigital.colourstest.dto.PersonUpdateDTO;
 import com.answerdigital.colourstest.exception.NotImplementedException;
@@ -80,6 +75,12 @@ public class PeopleController {
             responseEntity = new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
         return responseEntity;
+    }
+
+    @PostMapping
+    public ResponseEntity<Person> updatePerson(@RequestBody PersonUpdateDTO newPerson){
+        // TODO Optional 3
+        throw new NotImplementedException();
     }
 
 }
