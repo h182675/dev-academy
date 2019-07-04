11fedea8ca5bf33a3f67a12a8b51bb751ab15e68
Daniel pettersen <d.pettersen@gmail.com>
2019-07-04 15:04:31 +0200

    Implemented function for getting colours by id in ColoursController

--- dev-academy-java-backend/src/main/java/com/answerdigital/colourstest/controller/ColoursController.java
+++ dev-academy-java-backend/src/main/java/com/answerdigital/colourstest/controller/ColoursController.java
@@ -4,13 +4,12 @@ import com.answerdigital.colourstest.exception.NotImplementedException;
 import com.answerdigital.colourstest.model.Colour;
 import com.answerdigital.colourstest.repository.ColoursRepository;
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/colours")
@@ -25,9 +24,18 @@ public class ColoursController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Colour> getColour(){
+    public ResponseEntity<Colour> getColour(@PathVariable("id") long id){
         // TODO Optional 1
-        throw new NotImplementedException();
+
+        Optional<Colour> colourData = coloursRepository.findById(id);
+        ResponseEntity responseEntity;
+
+        if(colourData.isPresent()){
+            responseEntity = new ResponseEntity(colourData,HttpStatus.OK);
+        }else{
+            responseEntity = new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return responseEntity;
     }
 
     @PostMapping
