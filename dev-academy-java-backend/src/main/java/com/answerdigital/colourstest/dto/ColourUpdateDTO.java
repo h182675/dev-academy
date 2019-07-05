8f1a915446b0aba79f3d15780e74325b94b33552
Daniel pettersen <d.pettersen@gmail.com>
2019-07-05 14:53:22 +0200

    Created a ColoursUpdateDTO for the API entry point

---
+++ dev-academy-java-backend/src/main/java/com/answerdigital/colourstest/dto/ColourUpdateDTO.java
@@ -0,0 +1,63 @@
+package com.answerdigital.colourstest.dto;
+
+import java.util.Objects;
+
+public class ColourUpdateDTO {
+    private Long id;
+
+    private String name;
+
+    public ColourUpdateDTO(){
+
+    }
+
+    public ColourUpdateDTO(Long id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 59 * hash + Objects.hashCode(this.id);
+        hash = 59 * hash + Objects.hashCode(this.name);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final ColourUpdateDTO other = (ColourUpdateDTO) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+}
