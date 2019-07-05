116317607f2f74641d7463b86cb3a8af657e4eee
Daniel pettersen <d.pettersen@gmail.com>
2019-07-05 15:29:41 +0200

    Removed some unused imports

--- dev-academy-java-backend/src/test/java/com/answerdigital/colourstest/controller/ColoursControllerTest.java
+++ dev-academy-java-backend/src/test/java/com/answerdigital/colourstest/controller/ColoursControllerTest.java
@@ -15,9 +15,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
